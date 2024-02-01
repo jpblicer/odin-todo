f5cfbebf7b8c3274d196be6c25503b354fbc958c
jpblicer <jpblicer@gmail.com>
2024-02-01 21:03:13 +0900

    create project.js component with properties to store project title description and an array for todos

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import './style.css'
 import { setupCounter } from './counter.js'
 import TodoItem from './components/toDo.js'
-
+import Project from './components/project.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -21,3 +21,7 @@ setupCounter(document.querySelector('#counter'))
 // Example usage:
 const testTodo = new TodoItem("testName", "testDescript", "1990-01-01", "High");
 console.log(testTodo);
+
+const testProject = new Project("projectTitle", "Projectdescription", testTodo)
+
+console.table(testProject)
