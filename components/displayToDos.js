d4af1293284b8e0d1035a6675c3be328a9cc3f6b
jpblicer <jpblicer@gmail.com>
2024-02-03 19:55:40 +0900

    add if statement to displayToDos that will append or remove todos to each project

--- components/displayToDos.js
+++ components/displayToDos.js
@@ -1,9 +1,16 @@
-function displayToDos(Project, projectTitle) {
-    Project.todos.forEach(todo => {
-        const todosTitle = document.createElement("div");
-        todosTitle.innerText = todo.title;
-        projectTitle.appendChild(todosTitle);
-    });
+let showingToDos = true;
+
+function displayToDos(Project, projectTitle) {    
+    if(showingToDos){
+        Project.todos.forEach(todo => {
+            const todosTitle = document.createElement("div");
+            todosTitle.innerText = todo.title;
+            projectTitle.appendChild(todosTitle);
+        });
+    }else{
+        projectTitle.replaceChildren(projectTitle.firstChild);
+    }
+    showingToDos = !showingToDos;
 }
 
 export default displayToDos;
