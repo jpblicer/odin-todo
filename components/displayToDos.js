381e560d3f96ead8fccfb983f216ac7f290703b2
jpblicer <jpblicer@gmail.com>
2024-02-05 22:57:09 +0900

    create displayToDoDetails to append todo class constructors to the related todo

--- components/displayToDos.js
+++ components/displayToDos.js
@@ -5,6 +5,14 @@ function displayToDos(Project, projectTitle) {
         Project.todos.forEach(todo => {
             const todosTitle = document.createElement("div");
             todosTitle.innerText = todo.title;
+            
+            todosTitle.addEventListener('click', (event) => {
+                if (event.target === todosTitle) {
+                    event.stopPropagation();
+                    displayToDoDetails(Project, todosTitle, todo);
+                }
+            });
+
             projectTitle.appendChild(todosTitle);
         });
     }else{
@@ -14,3 +22,26 @@ function displayToDos(Project, projectTitle) {
 }
 
 export default displayToDos;
+
+
+let showingToDoDetails = true;
+
+function displayToDoDetails(Project, todosTitle, todo) { 
+    if (showingToDoDetails) {
+        const todosDescription = document.createElement("div");
+        const todosdueDate = document.createElement("div");
+        const todosPriority = document.createElement("div");
+
+        todosDescription.innerText = todo.description;
+        todosdueDate.innerText = todo.dueDate;
+        todosPriority.innerText = todo.priority;
+
+        todosTitle.appendChild(todosDescription);
+        todosTitle.appendChild(todosdueDate);
+        todosTitle.appendChild(todosPriority);
+    } else {
+        //todosTitle.replaceChildren(projectTitle.firstChild);
+        todosTitle.innerHTML = '';
+    }
+    showingToDos = !showingToDos;
+}
