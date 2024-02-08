8a247e945f2d8b4561fffbb2f51625b14d554155
jpblicer <jpblicer@gmail.com>
2024-02-08 22:18:37 +0900

    add id to add project button add event listener for click to that button which creates a new Project and console logs that project

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
 import './style.css'
 import displayProject from './components/displayProjects.js'
 import { testProjecta, testProjectb, Projects } from './components/Data.js';
+import Project from './components/project.js';
+
 
 document.querySelector('#app').innerHTML = `
   <div>
     <h1>Odin Project ToDo w/ Vite</h1>
     <button id="add-todo">Add ToDo</button>
-    <button>Add Project</button>
+    <button id="add-project">Add Project</button>
     <div id="project-container"></div>
   </div>
 `
 
 const projectContainer = document.querySelector("#project-container")
 const addTodoBtn = document.querySelector("#add-todo")
+const addProjectBtn = document.querySelector("#add-project")
+
+function addProject(){
+  const myProject = new Project("Project Title", "Project Description");
+  console.log(myProject)
+
+}
 
 addTodoBtn.addEventListener("click", testProjecta.addTodo.bind(testProjecta))
+addProjectBtn.addEventListener("click", addProject)
 
 
 
