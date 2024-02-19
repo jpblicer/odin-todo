964c8224b52ffb291dc616639acc2dd06c2eab91
jpblicer <jpblicer@gmail.com>
2024-02-19 21:27:52 +0900

    delete everything and start over with more simple approach. create input, button, and container. create variable to get each respective element add event listener to button that will append the text content of the input field to the todo container

--- components/Data.js
+++
@@ -1,7 +0,0 @@
-import Project from './project.js';
-
-const testProjecta = new Project("projectTitlea", "Projectdescription");
-const testProjectb = new Project("projectTitleb", "Projectdescription");
-const Projects = [testProjecta, testProjectb];
-
-export { testProjecta, testProjectb, Projects };
--- main.js
+++ main.js
@@ -1,50 +1,22 @@
 import './style.css'
-import displayProject from './components/displayProjects.js'
-import { testProjecta, testProjectb, Projects } from './components/Data.js';
-import Project from './components/project.js';
-
 
 document.querySelector('#app').innerHTML = `
   <div>
     <h1>Odin Project ToDo w/ Vite</h1>
-    <button id="add-todo">Add ToDo</button>
-    <button id="add-project">Add Project</button>
-    <div id="project-container"></div>
+    <div id="project-container">
+      <input id="inputTodo" type="text">
+      <button id="addTodo">+</button>
+      <div class = "todos" id="todoContainer">
+    </div>
   </div>
 `
 
-const projectContainer = document.querySelector("#project-container")
-const addTodoBtn = document.querySelector("#add-todo")
-const addProjectBtn = document.querySelector("#add-project")
+const addTodoBtn = document.getElementById("addTodo");
+const todoContainer = document.getElementById("todoContainer");
+const inputTodo = document.getElementById("inputTodo");
 
-function addProject(){
-  const myProject = new Project("Project Title", "Project Description");
-  console.log(myProject)
-
-}
-
-addTodoBtn.addEventListener("click", testProjecta.addTodo.bind(testProjecta))
-addProjectBtn.addEventListener("click", addProject)
-
-
-
-displayProject(Projects, projectContainer)
-
-
-/*
-TODO:
-Add empty array to store projects
-Create project function
-  creates new project from the project.js class
-  calls displayProjects after add
-
-
-Create todo function
-  update the project.js class add todo to prompt user for the necessary properties
-  calls add todo from project.js class
-
-append buttons to the dom to add inputs for 
-  the relevent class properties of todo and project
-
-remove Data.js
-*/
+addTodoBtn.addEventListener("click", function(){
+  let todo = document.createElement("p");
+  todo.innerText = inputTodo.value;
+  todoContainer.appendChild(todo);
+})
