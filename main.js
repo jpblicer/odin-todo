21936fccdc0ffecf18ad358c31d6b704e199f90b
jpblicer <jpblicer@gmail.com>
2024-02-19 22:42:04 +0900

    duplicate the functionality of todo as project remove todo from the html create function add todo move all todo elements into this function that are added to relevent project

--- main.js
+++ main.js
@@ -4,26 +4,67 @@ document.querySelector('#app').innerHTML = `
   <div>
     <h1>Odin Project ToDo w/ Vite</h1>
     <div id="project-container">
-      <input id="inputTodo" type="text">
-      <button id="addTodo">+</button>
-      <div class = "todos" id="todoContainer">
+      <input id="inputProject" type="text" placeholder="Add Project">
+      <button id="addProject">+</button>
+      <div class = "Project" id="projectContainer">
     </div>
   </div>
 `
 
-const addTodoBtn = document.getElementById("addTodo");
-const todoContainer = document.getElementById("todoContainer");
-const inputTodo = document.getElementById("inputTodo");
 
-addTodoBtn.addEventListener("click", function(){
-  let todo = document.createElement("p");
-  let todoRemoveBtn = document.createElement("button");
-  todoRemoveBtn.textContent = "-";
-  todo.innerText = inputTodo.value;
-  todo.append(todoRemoveBtn)
-  todoContainer.appendChild(todo);
+  const addProjectBtn = document.getElementById("addProject");
+  const projectContainer = document.getElementById("projectContainer");
+  const inputProject = document.getElementById("inputProject");
 
-  todoRemoveBtn.addEventListener("click", function(){
-    todo.remove(todo)
+  addProjectBtn.addEventListener("click", function(){
+    let project = document.createElement("h1");
+    let projectRemoveBtn = document.createElement("button");
+    projectRemoveBtn.textContent = "-";
+    project.innerText = inputProject.value;
+    project.append(projectRemoveBtn)
+    projectContainer.appendChild(project);
+
+    addTodo(project)
+    projectRemoveBtn.addEventListener("click", function(){
+      project.remove()
+    })
   })
-})
+
+
+
+
+
+function addTodo(project){
+
+  const inputTodo = document.createElement("input");
+  inputTodo.id = "inputTodo";
+  inputTodo.type = "text";
+  inputTodo.placeholder = "Add Todo";
+
+
+  const addTodoBtn = document.createElement("button");
+  addTodoBtn.textContent = "+";
+  addTodoBtn.id = "addTodo";
+
+  let todoContainer = document.createElement("div");
+  todoContainer.className = "todos";
+  todoContainer.id="todoContainer";
+
+  todoContainer.appendChild(inputTodo);
+  todoContainer.append(addTodoBtn)
+
+  project.appendChild(todoContainer)
+
+  addTodoBtn.addEventListener("click", function(){
+    let todo = document.createElement("p");
+    let todoRemoveBtn = document.createElement("button");
+    todoRemoveBtn.textContent = "-";
+    todo.innerText = inputTodo.value;
+    todo.append(todoRemoveBtn)
+    todoContainer.appendChild(todo);
+
+    todoRemoveBtn.addEventListener("click", function(){
+      todo.remove()
+    })
+  })
+};
