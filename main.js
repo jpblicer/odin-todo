4bbed29bce1a1376fb74ab8788f6100c2d0f22cd
jpblicer <jpblicer@gmail.com>
2024-02-25 19:29:28 +0900

    change let to const where possible. change todoClass to Todo to be more readable and clear change aTodo to newTodo move around variable declarations to make code more readable

--- main.js
+++ main.js
@@ -6,33 +6,32 @@ document.querySelector('#app').innerHTML = `
     <div id="project-container">
       <input id="inputProject" type="text" placeholder="Add Project">
       <button id="addProject">+</button>
-      <div class = "Project" id="projectContainer">
+      <div class = "Project" id="projectContainer"></div>
     </div>
   </div>
 `
-
-
   const addProjectBtn = document.getElementById("addProject");
   const projectContainer = document.getElementById("projectContainer");
   const inputProject = document.getElementById("inputProject");
 
 
-  addProjectBtn.addEventListener("click", function(){
-    let project = document.createElement("h2");
-    let projectRemoveBtn = document.createElement("button");
-    projectRemoveBtn.textContent = "-";
-    project.innerText = inputProject.value;
-    project.append(projectRemoveBtn)
-    projectContainer.appendChild(project);
+addProjectBtn.addEventListener("click", function(){
+  const project = document.createElement("h2");
+  const projectRemoveBtn = document.createElement("button");
+  projectRemoveBtn.textContent = "-";
+  project.innerText = inputProject.value;
+  project.append(projectRemoveBtn)
+  projectContainer.appendChild(project);
 
-    addTodo(project)
-    projectRemoveBtn.addEventListener("click", function(){
-      project.remove()
-    })
-  })
+  addTodo(project);
+
+  projectRemoveBtn.addEventListener("click", function(){
+    project.remove()
+  });
+});
 
 
-class todoClass{
+class Todo{
   constructor(todoTitle, todoDescription, todoDue, todoPriority){
     this.todoTitle = todoTitle;
     this.todoDescription = todoDescription;
@@ -53,35 +52,33 @@ function addTodo(project){
   addTodoBtn.textContent = "+";
   addTodoBtn.id = "addTodo";
 
-  let todoContainer = document.createElement("div");
+  const todoContainer = document.createElement("div");
   todoContainer.className = "todos";
   todoContainer.id="todoContainer";
 
   todoContainer.appendChild(inputTodo);
-  todoContainer.append(addTodoBtn)
-
+  todoContainer.appendChild(addTodoBtn)
   project.appendChild(todoContainer)
 
   addTodoBtn.addEventListener("click", function(){
-    let todo = document.createElement("p");
-    let todoRemoveBtn = document.createElement("button");
-    todoRemoveBtn.textContent = "-";
+    const todoTitle = inputTodo.value;
+    const todoDescription = prompt("Description:");
+    const todoDue = prompt("Due");
+    const todoPriority = prompt("Priority:")
+
+    const newTodo = new Todo(todoTitle, todoDescription, todoDue, todoPriority);
     
-    let todoTitle = inputTodo.value;
+    const todo = document.createElement("p");
     todo.innerText = todoTitle;
 
-    let todoDescription = prompt("description");
-    let todoDue = prompt("due");
-    let todoPriority = prompt("priority")
-
-    let aTodo = new todoClass(todoTitle, todoDescription, todoDue, todoPriority);
-    
-    
+    const todoRemoveBtn = document.createElement("button");
+    todoRemoveBtn.textContent = "-";
+        
     todo.addEventListener("click", function(){
-      alert(aTodo.todoTitle + " is due on " + aTodo.todoDue + " and has a priority of " + aTodo.todoPriority + " \n\n Description: \n\n" + aTodo.todoDescription)
+      alert(newTodo.todoTitle + " is due on " + newTodo.todoDue + " and has a priority of " + newTodo.todoPriority + " \n\n Description: \n\n" + newTodo.todoDescription)
     });
     
-    todo.append(todoRemoveBtn)
+    todo.appendChild(todoRemoveBtn)
     todoContainer.appendChild(todo);
 
     todoRemoveBtn.addEventListener("click", function(e){
